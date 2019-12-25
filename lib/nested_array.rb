af41cd63d37aa5d4de89359cb69d8393fea8853f
weng lin <wenglin@buffalo.edu>
2019-12-25 01:18:23 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,13 +17,15 @@ CONVENTIONAL_PRODUCE = [
 ]
 
 def assembled_matrix
-  [ORGANIC_PRODUCE,CONVENTIONAL_PRODUCE]# Build an array that contains both of the above arrays
+  [ORGANIC_PRODUCE,CONVENTIONAL_PRODUCE]
+  # Build an array that contains both of the above arrays
   # This matrix will represent a produce storage room
   # Organic standards require that organic products be stored ABOVE conventional, not the other way around
   # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
 end
 
 def sorted_matrix
+  [ORGANIC_PRODUCE.sort,CONVENTIONAL_PRODUCE.sort]
   # Using Array literal syntax only, build another nested array that
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character
